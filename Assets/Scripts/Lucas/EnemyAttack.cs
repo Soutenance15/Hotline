ded8cabf5f15334e825f0edd44e3a8dc28562c14
NinjYab <lucasclain097@gmail.com>
2025-10-28 15:25:15 +0400

    Enemy(patrol, attack, bullet) Health Script

---
+++ Assets/Scripts/Lucas/EnemyAttack.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public GameObject projectile;
+    public GameObject player;
+    public Transform shootPos;
+    public float timer;
+    public float shootDistance;
+    EnemyPatrol patrol;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootDistance = Vector2.Distance(transform.position, player.transform.position);
+    }
+
+    void FixedUpdate()
+    {
+         if (shootDistance < 5)
+        {
+            patrol = GetComponent<EnemyPatrol>();
+            patrol.StopMovement();
+            timer += Time.deltaTime;
+            if (timer > 1)
+            {
+                timer = 0;
+                Shoot();
+            }
+        }
+        else if (shootDistance > 5)
+        {
+            patrol = GetComponent<EnemyPatrol>();
+            patrol.ResumeMovement();
+        }
+    }
+
+     public void Shoot()
+        {
+            GameObject proj = Instantiate(projectile, shootPos.position, Quaternion.identity);
+        }
+}
---
+++ Assets/Scripts/Lucas/Health.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public int minHealth = 0;
+    public Slider healthBar;
+    public bool isAlive = true;
+   
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.maxValue = maxHealth;
+        healthBar.value = currentHealth;
+        healthBar.minValue = minHealth;
+       
+    }
+
+    public void UpdateHealthBar()
+    {
+        healthBar.value = currentHealth;
+    }
+
+    public void UpdateDamage(int damage)
+    {
+        currentHealth += damage;
+        UpdateHealthBar();
+        if (currentHealth <= 0)
+        {
+            isAlive = false;
+            Destroy(gameObject);
+        }
+    }
+}
